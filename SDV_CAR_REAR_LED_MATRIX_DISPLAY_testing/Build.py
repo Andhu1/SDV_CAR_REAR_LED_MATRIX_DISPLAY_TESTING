7d3ee65a485589ad7d4eb47871547b30cc967953
Andhu1 <48029656+Andhu1@users.noreply.github.com>
2025-07-10 17:50:44 +0530

    Update Build.py
    build.py

--- SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing/Build.py
+++ SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing/Build.py
@@ -4,10 +4,10 @@ import os
 # 🔧 Configurable Paths (Update these)
 UV4_PATH = r"C:\Keil_v5\UV4\UV4.exe"
 FROMELF_PATH = r"C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe"
-PROJECT_PATH = r"C:\Users\DELL\Downloads\UART_Project-main\UART_Project-main\SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing\Uart_project.uvprojx"
+PROJECT_PATH = r"C:\Users\DELL\.jenkins\workspace\SDV_demo1\SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing\Uart_project.uvprojx"
 TARGET_NAME = "Target 1"  # Use exact target name from Keil
-AXF_FILE = r"C:\Users\DELL\Downloads\UART_Project-main\UART_Project-main\SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing\Uart_project.axf"
-HEX_FILE = r"C:\Users\DELL\Downloads\UART_Project-main\UART_Project-main\SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing\Uart_project.hex"
+AXF_FILE = r"C:\Users\DELL\.jenkins\workspace\SDV_demo1\SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing\Uart_project.axf"
+HEX_FILE = r"C:\Users\DELL\.jenkins\workspace\SDV_demo1\SDV_CAR_REAR_LED_MATRIX_DISPLAY_testing\Uart_project.hex"
 
 def build_project():
     print("🚧 Building Keil project...")
